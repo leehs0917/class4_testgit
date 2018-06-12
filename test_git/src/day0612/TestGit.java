28099d3f0c7536544737908c0e869b2b350fa6a5
owner <owner@DESKTOP-ARKHM9J>
2018-06-12 15:39:26 +0900

    처음입니다. 잘 부탁 드립니다.

---
+++ test_git/src/day0612/TestGit.java
@@ -0,0 +1,11 @@
+package day0612;
+
+public class TestGit {
+
+	public static void main(String[] args) {
+		System.out.println("±êÇé¿¬½À");
+		int i = 28;
+		System.out.println("³ªÀÌ = "+i);
+	}//main
+
+}//class
